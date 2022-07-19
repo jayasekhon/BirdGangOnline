0bf410f68a5e06da2bc73fdd1560118b9429a276
jayasekhon <60359284+jayasekhon@users.noreply.github.com>
2022-07-19 16:55:19 +0100

    small updates

--- src/App.js
+++ src/App.js
@@ -16,30 +16,57 @@ function PDFFile() {
 
   function onDocumentLoadSuccess({ numPages }) {
     setNumPages(numPages);
+    setPageNumber(1);
   }
-  function goToPrevPage({ pageNumber }) {
-    setPageNumber(pageNumber - 1);
+  
+  function changePage(offset) {
+    setPageNumber(prevPageNumber => prevPageNumber + offset);
   }
-  function goToNextPage({ pageNumber }) {
-    setPageNumber(pageNumber + 1);
+  
+  function previousPage() {
+    changePage(-1);
+  }
+  
+  function nextPage() {
+    changePage(1);
   }
 
 
   return (
-    <div>
-      <nav>
-          <button onClick={this.goToPrevPage}>Prev</button>
-          <button onClick={this.goToNextPage}>Next</button>
-      </nav>
-    <div>
-      <Document file="/TeamSteam_BirdGang_Report.pdf" onLoadSuccess={onDocumentLoadSuccess} onLoadError={console.error}>
-        <Page pageNumber={pageNumber} />
+<>
+    <div className="main">
+      <Document
+        file={'TeamSteam_BirdGang_Report.pdf'}
+        onLoadSuccess={onDocumentLoadSuccess}
+      >
+        <Page pageNumber={pageNumber} scale = {0.8}/>
       </Document>
-      <p>
-        Page {pageNumber} of {numPages}
-      </p>
-    </div>
-    </div>
+      <div>
+        <div className="pagec">
+          Page {pageNumber || (numPages ? 1 : '--')} of {numPages || '--'}
+        </div>
+        <div className="buttonc">
+        <button
+          type="button"
+          disabled={pageNumber <= 1}
+          onClick={previousPage}
+          className="Pre"
+            
+        >
+          Previous
+        </button>
+        <button
+          type="button"
+          disabled={pageNumber >= numPages}
+          onClick={nextPage}
+           
+        >
+          Next
+        </button>
+        </div>
+      </div>
+      </div>
+    </>
   );
 }
 
@@ -119,12 +146,36 @@ function App(){
 
   <section id="about">
     <h1>ABOUT</h1>
-    <PDFFile/>
+  <div class='row'>
+    <div class='column'>
+      <div class='left-column'>
+      <p>BirdGang was created as part of the Unviersity of Bristol Computer Science M.Eng Games Project. It was awarded a first class mark, "judged as outstanding by the panel and entering truly professional territory". All aspects of the game were created in-house using the Unity Game Engine and Autodesk Maya. 
+      </p>
+      <p>
+      Players enter the world as BirdGang recruits and are tasked with finding bad people, pooping on them, and ruining their day. AI bird flocks populate the sky and target any players who remain still for too long. Players locate one another using markers showing teammates positions. During the game, players will face four miniboss rounds. In each, they must work together to defeat them, using waypoints and help messages to communicate with one another. Each of the two-minute rounds are introduced by a cinematic cutscene and an audio overlay from the gang boss. 
+      </p>
+      <p>
+      Source code for the project is available at: https://github.com/hamiltonrconnor/BirdGang
+      </p>
+      </div>
+    </div>
+    <div class='column'>
+      <div class='right-column'>
+        <PDFFile/>
+      </div>
+  </div>
+</div>
+    
 
   
   </section>
 
-  <section id="team"><h1>THE TEAM</h1></section>
+  <section id="team">
+    <h1>THE TEAM</h1>
+    <img src={'allbosses.png'}/>
+    
+  
+  </section>
 
   <section id="privacy"><h1>PRIVACY</h1></section>
 
